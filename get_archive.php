25c26a7cb3be6d0af9512f8e34d4a068481e0716
bernorieder <berno.rieder@gmail.com>
2016-11-29 09:18:19 +0000

    basic scraper script working, with some testing done

--- get_archive.php
+++ get_archive.php
@@ -1,23 +1,36 @@
 <?php
 
+// basic configuration, normally the only thing to modify
 $mainurl = "http://listserv.aoir.org/pipermail/air-l-aoir.org/";
-$mainsha = "/" . sha1($mainurl);
-
 $localpath = "/Applications/XAMPP/xamppfiles/htdocs/labs.polsys.net/tools/piperscraper";
+
+
+// basic script config
+ignore_user_abort(false);
+set_time_limit(3600*5);
+ini_set("memory_limit","500M");
+ini_set("error_reporting",1);
+
+// folder paramerters; TODO: put URL into parameter
+
 $jsonpath = "/json_mail";
+$shaid = "/" . sha1($mainurl);
+$jsondir = $localpath . $jsonpath . $shaid;
+
+// check for json directories, if not there make them
+if(!file_exists($localpath . $jsonpath)) { mkdir($localpath . $jsonpath); }
+if(!file_exists($jsondir)) { mkdir($jsondir); }
 
 
-// check for json directory, if not there make it
-$jsondir = $localpath . $jsonpath . $mainsha;
-
-//echo $jsondir; exit;
-if(!file_exists($jsondir)) {
-	mkdir($jsondir);
+// create a basic log file
+if(!file_exists($jsondir . ".log")) {
+	$content = "Archive URL: " . $mainurl . "\n";
+	$content .= "Scrape started: " . date("Y-m-d H:i:s",time());
+	file_put_contents($jsondir . ".log", $content);
 }
 
 
 // parse main archive page
-
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $mainurl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -27,71 +40,56 @@ if($content) {
 			
 	preg_match_all('/<A href="(.*)\/thread.html">/mi', $content, $current);
 	
-	echo "getting " . count($current[1]) . " months: ";
+	echo "getting " . count($current[1]) . " months:\n\n";
 	
 	$counter = 0;
 	foreach($current[1] as $link) {
 		
+		getMonth($link);
+		
 		$counter++;
-		
-		parseMonth($link);
-		
-		echo $counter . " ";  
+		echo "\n\n" . $counter . " " . $link . " ";
 	}
 }
 
 
-function parseMonth($month) {
+function getMonth($month) {
 	
 	global $mainurl,$jsondir;
 	
-	$link = $mainurl . $month."/thread.html";
+	$link = $mainurl . $month ."/thread.html";
 	
 	$ch = curl_init();
-
 	curl_setopt($ch, CURLOPT_URL, $link);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	
 	$content = curl_exec($ch);
 	
 	$content = preg_replace('/[\r\n]/', '', $content);
-	
-	//print_r($content);
-	
-	/*
-	<LI><A HREF="021865.html">[Air-L] Mobile Digital Interactive Storytelling 2nd cfp - New Review of Hypermedia and Multimedia
-	</A><A NAME="21865">&nbsp;</A>
-	<I>Cunliffe D J (AT)
-	</I>
-	*/	
-	
-	//preg_match_all('/<UL>(.*?)<\/UL>/', $content, $current);
-	
-	//print_r($current);
-	//exit;
-	
 	$content = preg_replace("/<\!--htdig_noindex-->/","",$content);
 	$content = preg_replace("/<\!--\/htdig_noindex-->/","",$content);
 	
-	preg_match_all('/<\!--(.*?) (.*?)- --><LI><A HREF="(.*?)">(.*?)<\/A>(.*?)<I>(.*?)<\/I>/i', $content, $current);
+	preg_match_all('/<\!--(.*?) (.*?)- --><LI><A HREF="(.*?)">(.*?)<\/A>(.*?)<I>(.*?)<\/I>/i', $content, $regthread);
 	
-	for($i = 0; $i < count($current[1]); $i++) {
+	echo count($regthread[1])." mails: ";
+	
+	for($i = 0; $i < count($regthread[1]); $i++) {
 		
-		if($current[1][$i] != 0) {
-			$thread = explode("-",$current[2][$i]);
+		echo $i . " ";
+		
+		if($regthread[1][$i] != 0) {
+			$thread = explode("-",$regthread[2][$i]);
 			$id = $thread[count($thread)-1];
 			$replytoid = $thread[count($thread)-2];
 		} else {
-			$id = $current[2][$i];
+			$id = $regthread[2][$i];
 			$replytoid = "base";
 		}
 		
-		
-		$author = $current[6][$i];
-		$subject = $current[4][$i];
+		$author = $regthread[6][$i];
+		$subject = $regthread[4][$i];
 		$text = "";
 		$date = date("Y-m-d H:i:s",strtotime($month));
-		$link = $mainurl . $month . "/" . $current[3][$i];
+		$link = $mainurl . $month . "/" . $regthread[3][$i];
 		
 		$link_sha = sha1($link);
 		
@@ -104,53 +102,35 @@ function parseMonth($month) {
 		$tmpjson["link"] = $link;
 		
 		$jsonfile = $jsondir . "/" . $tmpjson["id"] . ".json";
-		
 	
-		if(file_exists($jsonfile)) {
-			continue;
-		}
+		if(!file_exists($jsonfile)) {
 		
-		
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $link);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$content = curl_exec($ch);
-		
-		//echo $content; exit;
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $link);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$content = curl_exec($ch);
+			
+			if($content) {
 				
-		if($content) {
+	            $content = preg_replace('/[\r\n]/', ' ', $content);
+				
+				// parse date
+	            preg_match_all('/<I>(.*?)<\/I>/mi', $content, $regdate);
+				$newdate = date( 'Y-m-d H:i:s', strtotime($regdate[1][0]));
+				$tmpjson["date"] = $newdate;
+				
+				// parse mail body
+				preg_match_all('/<PRE>(.*?)<\/PRE>/mi', $content, $regtext);
+				$tmpjson["text"] = $regtext[1][0];
+				
+				sleep(0.1);
+			}
 			
-            $content = preg_replace('/[\r\n]/', ' ', $content);
-
-            preg_match_all('/<I>(.*?)<\/I>/mi', $content, $current);
-
-            // create format to parse: 10/Oct/2000:13:55:36 -0700
-            
-            $current[1][0] = preg_replace("/\s+/"," ",$current[1][0]);
-            
-            $timeels = explode(" ",$current[1][0]);
-
-            $thedate = $timeels[2] . "/" . $timeels[1] . "/" . $timeels[5] . ":" . $timeels[3] . " -0000";
-
+			$json = json_encode($tmpjson);
 			
-			$newdate = date( 'Y-m-d H:i:s', strtotime($thedate));
-			
-			$tmpjson["date"] = $newdate;
-			
-			
-			// get the mail body
-			preg_match_all('/<PRE>(.*?)<\/PRE>/mi', $content, $current);
-		
-			$tmpjson["text"] = addslashes($current[1][0]);
-			
-			sleep(1);
+			file_put_contents($jsonfile, $json);
 		}
-		
-		$json = json_encode($tmpjson);
-		
-		file_put_contents($jsonfile, $json);
 	}
-
 }
 
 ?>
